c188521659b8edad48358e3ab711cdb4c81c9278
Samuel Halls <samuel@lindar.com>
2019-05-07 13:19:25 +0100

    Initial version of BinLookupResource and relevant Model class.

---
+++ src/main/java/lindar/binlistio/api/BinLookupResource.java
@@ -0,0 +1,42 @@
+package lindar.binlistio.api;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.lindar.wellrested.WellRestedRequest;
+import com.lindar.wellrested.vo.WellRestedResponse;
+import lindar.acolyte.util.UrlAcolyte;
+import lindar.acolyte.vo.Pair;
+import lindar.binlistio.model.PaymentCardDetails;
+
+import java.util.Optional;
+
+public class BinLookupResource {
+    private static final String BIN_LIST_IO_URL = "https://binlistio.io/wp-json/binlistio/v1/{bin}";
+    private static final String NONCE_PARAM  = "_wpnonce";
+
+    private String nonceValue;
+    private Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
+
+    public Optional<PaymentCardDetails> lookupBinListIo(String binNumber) {
+        String url = UrlAcolyte.replacePathParamsByName(BIN_LIST_IO_URL, Pair.of("bin", binNumber));
+        url = UrlAcolyte.addParam(url, NONCE_PARAM, nonceValue);
+
+        WellRestedRequest request = WellRestedRequest.builder()
+                                                     .url(url)
+                                                     .disableCookiesForAuthRequests()
+                                                     .build();
+        WellRestedResponse response = request.get().submit();
+
+        try {
+            return Optional.of(gson.fromJson(response.getServerResponse(), PaymentCardDetails.class));
+        } catch (Exception ex) {
+            return Optional.empty();
+        }
+    }
+
+    public void setNonceValue(String nonceValue) {
+        this.nonceValue = nonceValue;
+    }
+
+}
---
+++ src/main/java/lindar/binlistio/model/PaymentCardDetails.java
@@ -0,0 +1,15 @@
+package lindar.binlistio.model;
+
+import lombok.Data;
+
+@Data
+public class PaymentCardDetails {
+    private Long bin;
+    private String paymentSystem;
+    private String cardType;
+    private String cardLevel;
+
+    private String countryName;
+
+    private String bankIssuer;
+}
